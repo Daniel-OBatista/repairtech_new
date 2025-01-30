bbc57c53703bff3350365b87e9b8b8d77391da58
Daniel-OBatista <daniel.celapp@gmail.com>
2025-01-30 00:04:57 -0300

    Update msg_footer.js

--- msg_footer.js
+++ msg_footer.js
@@ -1,16 +1,8 @@
 document.querySelector('#whatsappButton_2').addEventListener('click', function() {
     console.log('Botão WhatsApp clicado!');  // Verifique se o clique é detectado
 
-    // Obter os dados do formulário
-    const nome = document.getElementById('floatingNome').value;
-    const email = document.getElementById('floatingEmail').value;
-    const telefone = document.getElementById('floatingTelefone').value;
-    const preferencia = document.querySelector('select').value;
-    const satisfacao = document.getElementById('nivel-satisfacao').value;
-    const receberNovidades = document.getElementById('flexCheckDefault').checked ? "Sim" : "Não";
-
     // Criar a mensagem para o WhatsApp
-    const mensagem = `*Formulário de Contato*\n\nNome: ${nome}\nEmail: ${email}\nTelefone: ${telefone}\nPreferência de Contato: ${preferencia}\nNível de Satisfação: ${satisfacao}\nReceber Novidades por Email: ${receberNovidades}`;
+    const mensagem = `Olá, gostaria de mais informações.`;
 
     // Substitua 'seunumerowhatsapp' pelo número do WhatsApp
     const numeroWhatsApp = '5516982620915';
