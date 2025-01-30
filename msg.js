1968ff87dc0f35a9351b240a469451be1264d934
Daniel-OBatista <daniel.celapp@gmail.com>
2025-01-29 23:39:52 -0300

    botao

--- msg.js
+++ msg.js
@@ -1,4 +1,4 @@
-document.querySelector('.botao').addEventListener('click', function() {
+document.querySelector('.botao-padrao').addEventListener('click', function() {
     // Obter os dados do formulário
     const nome = document.getElementById('floatingNome').value;
     const email = document.getElementById('floatingEmail').value;
